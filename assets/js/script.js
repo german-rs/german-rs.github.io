d93e0bf71d21c6f517a2b4cf58198cc854324b43
german-rs <18427568+german-rs@users.noreply.github.com>
2024-06-27 09:57:41 -0400

    barra de navegación, intento 1 de botón sandwich

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ *  
+ * Autor: Germán A. Riveros Sepúlveda. 
+ * 
+ */
+
+// Función botón sandwich.
+function toggleMenu(){
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
